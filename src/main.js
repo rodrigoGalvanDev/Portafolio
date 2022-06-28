bddda5d2f10c423904a859287c76716e540946cd
nocxa <rodrigogalvann1@gmail.com>
2022-06-28 15:57:28 -0300

    Agrega nuevas funcionalidades

--- src/main.js
+++ src/main.js
@@ -1,15 +1,23 @@
-
+//Menu para responsive design
 const $menuNav = document.querySelector("#abrirMenu");
 const $menuMobile = document.querySelector("#mobileMenu");
 const $cerrarMenu = document.querySelector("#exitMenu")
 
-$cerrarMenu.onclick = function cerrarMenu(){
+$cerrarMenu.onclick = function cerrarMenuCruz(){
       $menuMobile.classList.remove("mostrar")
       $menuMobile.classList.add("ocultar")
 }
 
 $menuNav.onclick = function abrirMenu(){
-
       $menuMobile.classList.remove("ocultar");
       $menuMobile.classList.add("mostrar");
+}
+
+
+//Botones del menu mobile
+const $buttonsMobile = document.querySelector('#button-mb-menu')
+
+$buttonsMobile.onclick = function cerrarMenuButton(){
+      $menuMobile.classList.remove("mostrar")
+      $menuMobile.classList.add("ocultar")
 }
