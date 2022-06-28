5894e934c1cacb0cb17dfeff2cab8e87552d3143
nocxa <rodrigogalvann1@gmail.com>
2022-06-28 14:46:28 -0300

    agrega menu para responsive design

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
-history.scrollRestoration = "manual"
 
-$(window).on('beforeunload', function(){
-      $(window).scrollTop(0);
-});
+const $menuNav = document.querySelector("#abrirMenu");
+const $menuMobile = document.querySelector("#mobileMenu");
+const $cerrarMenu = document.querySelector("#exitMenu")
 
+$cerrarMenu.onclick = function cerrarMenu(){
+      $menuMobile.classList.remove("mostrar")
+      $menuMobile.classList.add("ocultar")
+}
+
+$menuNav.onclick = function abrirMenu(){
+
+      $menuMobile.classList.remove("ocultar");
+      $menuMobile.classList.add("mostrar");
+}
